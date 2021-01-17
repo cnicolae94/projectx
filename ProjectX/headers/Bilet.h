f67a0cf6439ab14fc7d112fbf5bc7971f7db6f3e
CLAUDIADANIELA NICOLAE <NICOLAECLAUDIA19@stud.ase.ro>
2021-01-17 17:43:03 +0200

    Added Bilet and modified main code

--- ProjectX/headers/Bilet.h
+++ ProjectX/headers/Bilet.h
@@ -1,15 +1,43 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include "Film.h"
+
 using namespace std;
 
 class Bilet
 {
 public:
-	void setCod(int cod);
+
+	Bilet();
+	Bilet(int, Film, int, int, int, float);
+
+	void setCod(int);
+	int getCod();
+
+	void setFilm(Film);
+	Film getFilm();
+
+	void setNrSala(int);
+	int getNrSala();
+
+	void setNrLoc(int);
+	int getNrLoc();
+
+	void setOraFilm(int);
+	int getOraFilm();
+
+	void setPret(float);
+	float getPret();
 
 private:
 	int cod;
-	//Film film;
+	Film film;
+	int nrSala;
+	int nrLoc;
+	int oraFilm;
+	float pret;
 	
 };
 
